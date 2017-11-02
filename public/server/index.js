3df92ea77eda8b9703f45ee23a0d823c4d1f75fe
Steven Bready <stevenbready@MacBook-Pro.local>
2017-11-02 09:07:34 -0600

    Completed Afternoon Project

--- public/server/index.js
+++ public/server/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const messages = require('./controllers/messages_controller')
 
+
 const app = express();
 app.use(bodyParser.json());
+app.use(express.static(__dirname + '/../build'))
 
 app.get('/api/messages', messages.read)
 app.post('/api/messages', messages.create)
@@ -11,5 +13,5 @@ app.put('/api/messages/:id', messages.update)
 app.delete('/api/messages/:id', messages.delete)
 
 
-const port = 3000;
+const port = 3001;
 app.listen(port, () => console.log(`Listening to port: ${port}`));
